812d247aa998b24b851df1eb155b981d1c16ab2d
Ria <157780826+ArianneCielo@users.noreply.github.com>
2025-11-18 14:57:07 +0800

    Create form_data.dart

---
+++ form_data.dart
@@ -0,0 +1,21 @@
+class FormData {
+  String? contactNumber;
+  String? civilStatus;
+  String? nationality;
+  String? email;
+  String? country;
+  String? schoolName;
+  String? yearLevel;
+  String? cluster;
+  String? gwa;
+  String? fatherName;
+  String? fatherOccupation;
+  String? motherName;
+  String? motherOccupation;
+  String? siblings;
+  String? familyIncome;
+  String? financialSupport;
+  String? monthlyExpenses;
+  String? reason;
+  List<String> uploadedFiles = [];
+}
